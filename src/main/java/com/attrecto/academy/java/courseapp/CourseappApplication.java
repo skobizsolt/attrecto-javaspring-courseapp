aaa788190780bdcb48f0be1f20f80c55b14efdf6
Erik Zsolt Skobrák <skobizsolt@gmail.com>
2022-09-26 19:39:52 +0200

    Második heti tananyag

---
+++ src/main/java/com/attrecto/academy/java/courseapp/CourseappApplication.java
@@ -0,0 +1,13 @@
+package com.attrecto.academy.java.courseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseappApplication.class, args);
+	}
+
+}
